292b81fc71c8227e993713de0d92a0e696946bde
Samuel Silva de Carvalho <samuel.silv.carvalho@gmail.com>
2022-08-17 08:48:14 -0300

    incluindo novas mensagens

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
+const { json } = require("express");
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors())
+app.use(json());
 
 let contagem = 0;
 app.post("/", async (req, res) => {
   try {
-    const nomeArquivo = contagem > 10 ? "vazio.xml" : "SEL1054R1.xml";
+    const nomeArquivo = contagem > 0 ? "vazio.xml" : "STR0016.xml";
     // const nomeArquivo = contagem > 10 ? "vazio.xml" : "SEL1611.xml";
 
     const arquivo = path.join(__dirname, nomeArquivo);
@@ -29,6 +34,18 @@ app.post("/", async (req, res) => {
   }
 });
 
-app.listen(4500, () => {
-  console.log(`🚀😍 Server running on port ${4500}`);
+app.post("/graphql", async (req, res) => {
+
+  const { body, headers } = req
+  console.log(body)
+  res.status(200).json({
+    body,
+    headers
+  });
+});
+
+const port = 6000;
+
+app.listen(port, () => {
+  console.log(`🚀😍 Server running on port ${port}`);
 });
